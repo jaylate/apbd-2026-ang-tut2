30a04b1f9406cbe613673d53716c19b91d2409f0
jaylate <178095084+jaylate@users.noreply.github.com>
2026-03-10 09:39:34 +0100

    Add basic input handling

--- Tutorial2/StatisticsHelper.cs
+++ Tutorial2/StatisticsHelper.cs
@@ -2,5 +2,12 @@ namespace Tutorial2;
 
 public class StatisticsHelper
 {
-
+	public static int Main(string[] args)
+	{
+		string line;
+		while ((line = Console.ReadLine()) != null) {
+			Console.WriteLine(line);
+		}
+		return 0;
+	}
 }
